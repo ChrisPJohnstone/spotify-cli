eedbe8d2a7d8e4ba65076219f23049e42ddd9f46
Christopher Johnstone <chrisj97@proton.me>
2025-05-17 12:51:24 +0100

    Added player_next tests

---
+++ src/cli/tests/test_player_next.py
@@ -0,0 +1,16 @@
+from argparse import Namespace
+from unittest.mock import MagicMock, call, patch
+
+from src.cli.player_next import PlayerNext
+
+FILEPATH: str = "src.cli.player_next"
+
+
+def test_parent_parsers() -> None:
+    assert PlayerNext.parent_parsers() == []
+
+
+@patch(f"{FILEPATH}.Spotify")
+def test_init(mock_spotify: MagicMock) -> None:
+    PlayerNext(Namespace())
+    mock_spotify.assert_has_calls([call(), call().next()])
